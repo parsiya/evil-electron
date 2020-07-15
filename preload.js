46d51fc54fea2cb841644b5cf08858428217c1bb
parsiya <parsiya@gmail.com>
2020-07-15 12:04:06 -0700

    initial mac and linux support

--- preload.js
+++ preload.js
@@ -1,2 +1,34 @@
-// Spawn a command prompt.
-require('child_process').exec('start cmd.exe');
+// Detect the operating system.
+import { platform } from "process";
+
+// Commands
+const win32Command = "start cmd.exe";
+const darwinCommand = "'/System/Applications/Terminal.app/Contents/MacOS/Terminal',function(){}";
+const linuxCommand = "/bin/bash";
+
+var command = "";
+
+switch (platform) {
+    case "win32":
+        command = win32Command;
+        break;
+    case "darwin":
+        command = darwinCommand;
+        break;
+    case "linux":
+        command = linuxCommand;
+        break;
+}
+
+if (command === "") {
+    console.log(`Operating system '${platform}' is not supported.`);
+} else {
+    // Spawn a command prompt.
+    require('child_process').exec(command);
+}
+
+
+
+
+
+
